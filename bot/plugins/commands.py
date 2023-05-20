0db50ba6b147c7b69f93635cc336cfb704fd3640
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-05-20 08:46:00 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -27,21 +27,13 @@ async def start(bot, update):
                 return
         except UserNotParticipant :
             await message.reply_text(
-                text="if force_channel:
-        try:
-            user = await client.get_chat_member(force_channel, message.from_user.id)
-            if user.status == "kicked out":
-                await message.reply_text("You Are Banned")
-                return
-        except UserNotParticipant :
-            await message.reply_text(
                 text="🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭.\n\nനിങ്ങൾക്ക് മൂവീസ് വേണോ? എങ്കിൽ തായെ കാണുന്ന ഞങ്ങളുടെ മെയിൻ ചാനലിൽ ജോയിൻ ചെയ്യുക.😂\nഎന്നിട്ട് ഗ്രൂപ്പിൽ പോയി വീണ്ടും മൂവിയിൽ ക്ലിക് ചെയ്ത് start കൊടുത്തു നോക്കൂ..!😁",
                 reply_markup=InlineKeyboardMarkup( [[
                  InlineKeyboardButton("🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭", url=f"t.me/{force_channel}")
                  ]]
                  )
             )
-            return",
+            return,
    
     try:
         file_uid = update.command[1]
