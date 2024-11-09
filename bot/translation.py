a1b28c6532b6f909393b9854e5a54561390b6c4a
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-11-09 19:48:31 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -2,52 +2,32 @@
 
 class Translation(object):
     
-    START_TEXT = """Hey {} 👋
+    START_TEXT = """
+<b> Hᴇʏ ᴛʜᴇʀᴇ {} 👋,
     
-𝖬𝗒 𝖭𝖺𝗆𝖾 𝗂𝗌 [𝖱𝖾𝗌𝗆𝗂 𝖱 𝖭𝖺𝗂𝗋 🦋](t.me/CCResmiBot), 𝖨 𝖺𝗆 𝖺𝗇 𝖠𝖽𝗏𝖺𝗇𝖼𝖾𝖽 𝖠𝗎𝗍𝗈 𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍 𝗐𝗈𝗋𝗄𝗂𝗇𝗀 𝖿𝗈𝗋 [𝖢𝗂𝗇𝖾𝗆𝖺 𝖢𝖺𝖿𝖾 🦋](t.me/CC_LinkzzTG). 𝖸𝗈𝗎 𝖼𝖺𝗇 𝖺𝗅𝗌𝗈 𝗎𝗌𝖾 𝗆𝖾, 𝖩𝗎𝗌𝗍 𝖺𝖽𝖽 𝗆𝖾 𝗍𝗈 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉 𝖺𝗇𝖽 𝖾𝗇𝗃𝗈𝗒 😍"""
+Mʏ Nᴀᴍᴇ Is [Sʜʀᴀᴅᴅʜᴀ Kᴀᴘᴏᴏʀ ✨](t.me/PrimeShraddhaBot)
 
+I Aᴍ ᴀ Mᴏᴠɪᴇ Pʀᴏᴠɪᴅɪɴɢ Mᴀᴄʜɪɴᴇ Fᴏʀ Pʀɪᴍᴇ Mᴏᴠɪᴇᴢ Gʀᴏᴜᴘs, Oɴʟʏ Aᴜᴛʜᴏʀɪᴢᴇᴅ Aᴅᴍɪɴs Cᴀɴ Usᴇ Mᴇ Sᴏ Dᴏɴ'ᴛ Wᴀsᴛᴇ Yᴏᴜʀ Tɪᴍᴇ 😁 
+
+Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :- @YourPrimeTG </b>"""
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b> Hᴇʏ {} 👋,
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 : [×‌× 𝙼𝚛. 𝙷𝙺𝚉 𝚃𝙶 🇮🇳࿐](t.me/MR_HKZ_TG)
+Sᴇᴇ Yᴏᴜ Iɴ [Gʀᴏᴜᴘ](t.me/PrimeXLinkzz) കുട്ടാ..😁 </b>
 """
     
-    ABOUT_TEXT = """🤖 𝖬𝗒 𝖭𝖺𝗆𝖾 : [𝖱𝖾𝗌𝗆𝗂 𝖱 𝖭𝖺𝗂𝗋 🦋](t.me/CCResmiBot)
-👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 : [×‌× 𝙼𝚛. 𝙷𝙺𝚉 𝚃𝙶 🇮🇳࿐](t.me/MR_HKZ_TG)
-♻️ 𝖬𝖺𝗂𝗇𝗍𝖺𝗂𝗇𝖾𝖽 𝖡𝗒 : [𝖢𝗂𝗇𝖾𝗆𝖺 𝖢𝖺𝖿𝖾 🦋](t.me/CC_LinkzzTG)
-🌐 𝖡𝗈𝗍 𝖲𝖾𝗋𝗏𝖾𝗋 : [𝖱𝖾𝗇𝖽𝖾𝗋](https://render.com)
-📃 𝖲𝗈𝗎𝗋𝖼𝖾 𝖢𝗈𝖽𝖾 : [𝖠𝗌𝗄 𝗁𝗂𝗆 🛡](t.me/MR_HKZ_TG)"""
+    ABOUT_TEXT = """<b>
+✯ Mʏ Nᴀᴍᴇ : Sʜʀᴀᴅᴅʜᴀ Kᴀᴘᴏᴏʀ
+✯ Dᴇᴠᴇʟᴏᴘᴇʀ : [𓊈𒆜 𝙼𝚛.ᴘʀɪᴍᴇ 𝚃𝙶 𒆜𓊉](t.me/YourPrimeTG)
+✯ Lᴀɴɢᴜᴀɢᴇ : [Pʏᴛʜᴏɴ 3.13.0](www.python.org)
+✯ Lɪʙʀᴀʀʏ : [Pʏʀᴏɢʀᴀᴍ](https://docs.pyrogram.org/)
+✯ Sᴇʀᴠᴇʀ : [Sᴏᴍᴇᴡʜᴇʀᴇ](www.goandlearn.com)
+✯ Dᴀᴛᴀʙᴀsᴇ : [Mᴏɴɢᴏ DB](www.mongodb.com) </b>"""
     
 
 
-    STATUS_TEXT = """★ Total Files: {}
-★ Using Storage: {} MB
-★ Free Storage: {} MB"""
+    STATUS_TEXT = """<b>
+★ Tᴏᴛᴀʟ Fɪʟᴇs: {}
+★ Usɪɴɢ Sᴛᴏʀᴀɢᴇ: {} MB
+★ Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: {} MB </b>"""
