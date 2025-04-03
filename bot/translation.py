370e4d3f31d090154f194411dea5972fd4c02bc5
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2025-04-03 20:27:59 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,21 +5,21 @@ class Translation(object):
     START_TEXT = """
 <b> Hᴇʏ ᴛʜᴇʀᴇ {} 👋,
     
-Mʏ Nᴀᴍᴇ Is [Sʜʀᴀᴅᴅʜᴀ Kᴀᴘᴏᴏʀ ✨](t.me/PrimeShraddhaBot)
+Mʏ Nᴀᴍᴇ Is [Rᴀsʜᴍɪᴋᴀ Mᴀɴᴅᴀɴɴᴀ ✨](t.me/MalluRashmikaRobot)
 
-I Aᴍ ᴀ Mᴏᴠɪᴇ Pʀᴏᴠɪᴅɪɴɢ Mᴀᴄʜɪɴᴇ Fᴏʀ Pʀɪᴍᴇ Mᴏᴠɪᴇᴢ Gʀᴏᴜᴘs, Oɴʟʏ Aᴜᴛʜᴏʀɪᴢᴇᴅ Aᴅᴍɪɴs Cᴀɴ Usᴇ Mᴇ Sᴏ Dᴏɴ'ᴛ Wᴀsᴛᴇ Yᴏᴜʀ Tɪᴍᴇ 😁 
+I Aᴍ ᴀ Mᴏᴠɪᴇ Pʀᴏᴠɪᴅɪɴɢ Mᴀᴄʜɪɴᴇ Fᴏʀ [Mᴀʟʟᴜ Lɪɴᴋᴢᴢ Gʀᴏᴜᴘs](t.me/MalluXLinkzz), Oɴʟʏ Aᴜᴛʜᴏʀɪᴢᴇᴅ Aᴅᴍɪɴs Cᴀɴ Usᴇ Mᴇ Sᴏ Dᴏɴ'ᴛ Wᴀsᴛᴇ Yᴏᴜʀ Tɪᴍᴇ 😁 
 
-Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :- @YourPrimeTG </b>"""
+Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :- @MR_TONY_99 </b>"""
     
     HELP_TEXT = """
 <b> Hᴇʏ {} 👋,
 
-Sᴇᴇ Yᴏᴜ Iɴ [Gʀᴏᴜᴘ](t.me/PrimeXLinkzz) കുട്ടാ..😁 </b>
+Sᴇᴇ Yᴏᴜ Iɴ [Mᴀʟʟᴜ Lɪɴᴋᴢᴢ](t.me/MalluXLinkzz) കുട്ടാ..😁 </b>
 """
     
     ABOUT_TEXT = """<b>
-✯ Mʏ Nᴀᴍᴇ : Sʜʀᴀᴅᴅʜᴀ Kᴀᴘᴏᴏʀ
-✯ Dᴇᴠᴇʟᴏᴘᴇʀ : [𓊈𒆜 𝙼𝚛.ᴘʀɪᴍᴇ 𝚃𝙶 𒆜𓊉](t.me/YourPrimeTG)
+✯ Mʏ Nᴀᴍᴇ : [Rᴀsʜᴍɪᴋᴀ Mᴀɴᴅᴀɴɴᴀ ✨](t.me/MalluRashmikaRobot)
+✯ Dᴇᴠᴇʟᴏᴘᴇʀ : [Tᴏɴʏ Sᴛᴀʀᴋ](t.me/MR_TONY_99)
 ✯ Lᴀɴɢᴜᴀɢᴇ : [Pʏᴛʜᴏɴ 3.13.0](www.python.org)
 ✯ Lɪʙʀᴀʀʏ : [Pʏʀᴏɢʀᴀᴍ](https://docs.pyrogram.org/)
 ✯ Sᴇʀᴠᴇʀ : [Sᴏᴍᴇᴡʜᴇʀᴇ](www.goandlearn.com)
